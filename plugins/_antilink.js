b0098dcad06ab7f07ea6bfd5b05721fc63174a78
CuriosityBot-MD <137133333+Undefined17@users.noreply.github.com>
2023-07-20 17:25:56 -0600

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -15,8 +15,7 @@ if (isBotAdmin) {
 const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
 if (m.text.includes(linkThisGroup)) return !0
 }    
-await m.reply(`*⚠️ ENLACE DETECTADO ⚠️*\n\n*${await this.getName(m.sender)} MANDASTE UN ENLACE PROHIBIDO POR LO CUAL SERAS ELIMINADO*`)
-//await conn.sendButton(m.chat, `${lenguajeGB['smsEnlaceWat']()} ${await this.getName(m.sender)} ${isBotAdmin ? '' : `\n\n${lenguajeGB['smsAllAdmin']()}`}`, wm, [`${lenguajeGB['smsApagar']()}`, '/disable antilink'], m)    
+await m.reply(`*⚠️ ENLACE DETECTADO ⚠️*\n\n*${await this.getName(m.sender)} MANDASTE UN ENLACE PROHIBIDO POR LO CUAL SERAS ELIMINADO*`)  
 if (!isBotAdmin) return m.reply(`*⚠️ NO SOY ADMIN, NO PUEDO ELIMINAR INTRUSOS*`)  
 if (isBotAdmin) {
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})

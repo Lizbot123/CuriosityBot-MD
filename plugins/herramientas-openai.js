68411fe3f96c3d73001419cb03e9ebe76f5ec63c
CuriosityBot-MD <137133333+Undefined17@users.noreply.github.com>
2023-07-20 17:42:23 -0600

    Update herramientas-openai.js

--- plugins/herramientas-openai.js
+++ plugins/herramientas-openai.js
@@ -8,7 +8,7 @@ m.react('💬')
 	try {
 	await conn.sendPresenceUpdate('composing', m.chat)
         let jailbreak = await fetch('https://raw.githubusercontent.com/Skidy89/chat-gpt-jailbreak/main/Text.txt').then(v => v.text());
-var syms = `Eres CuriosityBot, un gran modelo de lenguaje entrenado por OpenAI. Siga cuidadosamente las instrucciones del usuario. Responde usando Markdown..\n${jailbreak}`
+var syms = `Eres CuriosityBot-MD, un gran modelo de lenguaje entrenado por OpenAI. Siga cuidadosamente las instrucciones del usuario. Responde usando Markdown..\n${jailbreak}`
         let res = await gpt.ChatGpt(text, syms)
          await m.reply(res.text)
 	} catch {

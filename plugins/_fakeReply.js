629a66ee14244132c834c41ca1c49bf422c488cb
CuriosityBot-MD <137133333+Undefined17@users.noreply.github.com>
2023-07-25 14:14:37 -0600

    Update _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -2,7 +2,6 @@ import fs from 'fs'
 import fetch from 'node-fetch'
 import axios from 'axios'
 import moment from 'moment-timezone'
-import knights from 'knights-canvas'
 
 let handler = m => m
 handler.all = async function (m) {

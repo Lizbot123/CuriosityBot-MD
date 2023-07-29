35863ffd5022bdbd39e3b278fa1415174d3820ee
Azami ofc <137133333+Undefined17@users.noreply.github.com>
2023-07-29 00:37:16 -0600

    © CuriosityBot-MD

--- plugins/_Allfake.js
+++ plugins/_Allfake.js
@@ -5,6 +5,26 @@ import moment from 'moment-timezone'
 
 let handler = m => m
 handler.all = async function (m) {
+
+	global.getBuffer = async function getBuffer(url, options) {
+	try {
+		options ? options : {}
+		var res = await axios({
+			method: "get",
+			url,
+			headers: {
+				'DNT': 1,
+				'User-Agent': 'GoogleBot',
+				'Upgrade-Insecure-Request': 1
+			},
+			...options,
+			responseType: 'arraybuffer'
+		})
+		return res.data
+	} catch (e) {
+		console.log(`Error : ${e}`)
+	}
+	}
 	
 	let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
 	let pp = await this.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/1861aab98389b13db8588.jpg')

5ccaa5729c3dcfc1ddae6cc57d060d6991b3fec7
Azami ofc <137133333+Undefined17@users.noreply.github.com>
2023-07-29 02:37:03 -0600

    © CuriosityBot-MD

--- plugins/descargas-pinterest.js
+++ plugins/descargas-pinterest.js
@@ -1,12 +1,13 @@
 import { pinterest } from '@bochilteam/scraper'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
   if (!text) throw `*⚠️ INGRESE UN TEXTO*\n\n❕ EJEMPLO\n*${usedPrefix + command} Minecraft*`
-  m.react('🚀')
+  m.react('⏰')
   const json = await pinterest(text)
   conn.sendFile(m.chat, json.getRandom(), 'pinterest.jpg', `
 📍 *Resultado de:* ${text}
 🔎 *Buscado en:* Pinterest
-`.trim(), fliveLoc2, m)
+
+© Curiosity`.trim(), fliveLoc, m)
 }
 handler.help = ['pinterest <keyword>']
 handler.tags = ['dl']

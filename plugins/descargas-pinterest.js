0f7f33a7dbe30d188afb008e59720f0532b7eeae
Azami ofc <137133333+Undefined17@users.noreply.github.com>
2023-07-29 02:29:22 -0600

    Update descargas-pinterest.js

--- plugins/descargas-pinterest.js
+++ plugins/descargas-pinterest.js
@@ -1,5 +1,4 @@
 import { pinterest } from '@bochilteam/scraper'
-//let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let handler = async(m, { conn, text, usedPrefix, command }) => {
   if (!text) throw `*⚠️ INGRESE UN TEXTO*\n\n❕ EJEMPLO\n*${usedPrefix + command} Minecraft*`
   m.react(rwait)

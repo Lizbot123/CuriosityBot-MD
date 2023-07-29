328ab3540b32329798e6c73487ad55cacd09d1fd
Azami ofc <137133333+Undefined17@users.noreply.github.com>
2023-07-28 21:47:49 -0600

    © CuriosityBot-MD

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -11,7 +11,7 @@ let _muptime
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `🥏 Soy ${cb} | ⏰ Activo durante: ${muptime}\n\n | 🌟 Creado por Azami `
+		let bio = `🥏 Soy ${cb} | ⏰ Activo durante: ${muptime}\n\n | 🎨 Creado por Azami `
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 

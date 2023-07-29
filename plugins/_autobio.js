1b8998b40868929211f5d15c1db67813b2fa9284
Azami ofc <137133333+Undefined17@users.noreply.github.com>
2023-07-28 21:47:26 -0600

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -11,7 +11,7 @@ let _muptime
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `🥏 Soy ${wm} | ⏰ Activo durante: ${muptime}\n\n | 🌟 Creador: Azami `
+		let bio = `🥏 Soy ${cb} | ⏰ Activo durante: ${muptime}\n\n | 🌟 Creado por Azami `
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 

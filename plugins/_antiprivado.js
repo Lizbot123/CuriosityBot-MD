7e29b1a8d22dd344be2ee7f903a438efe418a9da
Azami ofc <137133333+Undefined17@users.noreply.github.com>
2023-07-28 18:13:24 -0600

    © CuriosityBot-MD

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 let user = global.db.data.users[m.sender]
 let bot = global.db.data.settings[this.user.jid] || {}
 if (bot.antiPrivate && !isOwner && !isROwner) {
-await m.reply(`*⚠️ Hola @${m.sender.split`@`[0]}, NO PUEDE USAR ESTE BOT EN CHAT PRIVADO*\n\nUnete al Grupo oficial para poder usar el bot\n${n}`, false, { mentions: [m.sender] })
+await m.reply(`*⚠️ Hola @${m.sender.split`@`[0]}, NO PUEDE USAR ESTE BOT EN CHAT PRIVADO*\n\nUnete al Grupo oficial para poder usar el bot\n${nn}`, false, { mentions: [m.sender] })
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'banchat')
 return !1
 }}

db04d6a22e967e4172d975b1ecfb01832a39ae19
Azami ofc <137133333+Undefined17@users.noreply.github.com>
2023-07-29 17:40:09 -0600

    © CuriosityBot-MD

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -8,28 +8,28 @@ let handler = async (m) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
 let name = await conn.getName(who)
-  let q = m.quoted ? m.quoted : m
-  let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw '*⚠️ RESPONDA A UNA IMAGEN*'
-	m.react(done)
-  let media = await q.download()
-  let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
-  let link = await (isTele ? uploadImage : uploadFile)(media)
-  let info = ` *🗂️ ENLACE:*\n${link}
-  *⚖️ TAMAÑO:*\n${media.length} bytes\n
-  *🚀 EXPIRACION:*\n ${isTele ? '✅ NO EXPIRA' : '⚠️ DESCONOCIDO'}\n
-  *🔰 ACORTADO:*\n${await shortUrl(link)}`
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+if (!mime) throw '*⚠️ RESPONDA A UNA IMAGEN*'
+m.react(done)
+let media = await q.download()
+let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
+let link = await (isTele ? uploadImage : uploadFile)(media)
+let info = ` *🗂️ ENLACE:*\n${link}
+*⚖️ TAMAÑO:*\n${media.length} bytes\n
+*🚀 EXPIRACION:*\n ${isTele ? '✅ NO EXPIRA' : '⚠️ DESCONOCIDO'}\n
+*🔰 ACORTADO:*\n${await shortUrl(link)}`
 
 conn.reply(m.chat, info, m, { contextInfo: {
-          externalAdReply :{
-    mediaUrl: ig, 
-    mediaType: 2,
-    title: wm,
-    body: azami,
-    thumbnail: await(await fetch(link)).buffer(),
-    sourceUrl: link
-     }}
-  })
+externalAdReply :{
+mediaUrl: ig, 
+mediaType: 2,
+title: wm,
+body: azami,
+thumbnail: await(await fetch(link)).buffer(),
+sourceUrl: link
+}}
+})
 }
 handler.help = ['tourl']
 handler.tags = ['convertido']

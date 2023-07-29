4c02f3e7cc96ddace6176f726f5c623c2ae80a8e
Azami ofc <137133333+Undefined17@users.noreply.github.com>
2023-07-29 10:31:22 -0600

    © CuriosityBot-MD

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -1,4 +1,30 @@
-import instagramGetUrl from 'instagram-url-direct'
+import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper' 
+ let handler = async (m, { conn, args, usedPrefix, command }) => { 
+   if (!args[0]) throw `*⚠️ INGRESA UN ENLACE DE INSTAGRAM*\n\n💡 EJEMPLO\n*${usedPrefix + command} https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=*` 
+   try { 
+     await m.reply('_*🚀 C A R G A N D O*_') 
+     const results = await instagramdl(args[0]) 
+       .catch(async _ => await instagramdlv2(args[0])) 
+       .catch(async _ => await instagramdlv3(args[0])) 
+       .catch(async _ => await instagramdlv4(args[0])) 
+     for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', global.wm, m) 
+   } catch (e) { 
+     console.log(e) 
+     m.reply(`*⚠️ ERROR, NO FUE POSIBLE DESCARGAR SU PEDIDO*`) 
+   } 
+ } 
+  
+ handler.help = ['ig'].map(v => v + ' <url>') 
+ handler.tags = ['downloader'] 
+  
+ handler.command = ^(Instagram|ig|igdl)$/i
+ handler.limit = true 
+ handler.register = true 
+  
+ export default handler
+
+
+/*import instagramGetUrl from 'instagram-url-direct'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `·˚ ༘₊· ͟͟͞͞꒰➳ 𝚄𝚜𝚘 𝚍𝚎𝚕 𝚌𝚘𝚖𝚊𝚗𝚍𝚘\n𝙴𝚓𝚎𝚖𝚙𝚕𝚘: *${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`
     m.react(rwait)
@@ -12,4 +38,4 @@ handler.tags = ['dl']
 
 handler.command = /^(Instagram|ig|igdl)$/i
 handler.diamond = true
-export default handler 
+export default handler*/

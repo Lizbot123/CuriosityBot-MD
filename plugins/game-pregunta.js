1b13698bd6c268bbf57a8a4d642c90d6739f418d
Azami ofc <137133333+Undefined17@users.noreply.github.com>
2023-07-29 23:36:54 -0600

    © CuriosityBot-MD

--- plugins/game-pregunta.js
+++ plugins/game-pregunta.js
@@ -1,11 +1,9 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
- if (!text) throw `♦︎ *Ejemplo :*\n\n *${usedPrefix + command}* Hoy van llover?`
-  m.react('🫣') 
- m.reply(`
-*⁉️ 𝐏𝐑𝐄𝐆𝐔𝐍𝐓𝐀𝐒 ⁉️*
-  
-*𝙿𝚁𝙴𝙶𝚄𝙽𝚃𝙰:* ${text}
-*𝚁𝙴𝚂𝙿𝚄𝙴𝚂𝚃𝙰:* ${['Si','Tal vez sí','Posiblemente','Probablemente no','No','Imposible'].getRandom()}
+if (!text) throw `*⚠️ INGRESE UN TEXTO A PREGUNTAR*\n\n💡 EJEMPLO:\n\n *${usedPrefix + command}* Hoy va llover?`
+m.react('🤔') 
+m.reply(`👀 P R E G U N T A S 👀  
+*Pregunta:* ${text}
+*Respuesta:* ${['Si','Tal vez sí','Posiblemente','Probablemente no','No','Imposible', 'No sabría decirte'].getRandom()}
 `.trim(), null, m.mentionedJid ? {
 mentions: m.mentionedJid
 } : {})}

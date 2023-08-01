df1cd189dbb96907c3467f43ccfdca757bb23926
Azami <137133333+Undefined17@users.noreply.github.com>
2023-08-01 14:32:58 -0600

    © CuriosityBot-MD

--- plugins/game-gay.js
+++ plugins/game-gay.js
@@ -8,7 +8,7 @@ global.API("https://some-random-api.com", "/canvas/gay", {
 avatar: await conn.profilePictureUrl(who, "image").catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png"),
 }),
 "error.png",
-"*Esta persona es orgullosamente gay ❤️🏳️‍🌈*",
+"🌈 *LGBT+ 🏳️‍🌈*",
 m
 )
 }

dd5531566eabeefb6c1fe2e1ba487bba4addd208
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-08-01 23:52:11 -0500

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -1,4 +1,4 @@
-/*import { sticker } from '../lib/sticker.js'
+import { sticker } from '../lib/sticker.js'
 import uploadFile from '../lib/uploadFile.js'
 import uploadImage from '../lib/uploadImage.js'
 import { webp2png } from '../lib/webp2mp4.js'
@@ -52,47 +52,5 @@ handler.exp = 200
 export default handler
 
 const isUrl = (text) => {
-return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))}*/
-
-import { sticker } from '../lib/sticker.js'
-import uploadFile from '../lib/uploadFile.js'
-import uploadImage from '../lib/uploadImage.js'
-import { webp2png } from '../lib/webp2mp4.js'
-
-let handler = async (m, { conn, args, usedPrefix, command, text }) => {
-let stiker = false
-let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || q.mediaType || ''
-
-if (!/webp|image|video/g.test(mime) && !text) return m.reply(`*⚠️ RESPONDE A UNA IMAGEN O VIDEO CON ${usedPrefix + command}*`)
-if (/video/g.test(mime)) if ((q.msg || q).seconds > 10) return m.reply('*⚠️ EL VÍDEO NO PUEDE DURAR MAS DE 10 SEGUNDOS*')
-
-if (/webp|image|video/g.test(mime)) {
-let img = await q.download?.()
-let out
-stiker = await sticker(img, false, global.packname, global.author)
-  
-if (!stiker) {
-await conn.reply(m.chat, `_Calma crack estoy haciendo tu sticker 👏_\n\n_*Recuerda los stickersgif son de 6 segundos*_\n\n_*by CuriosityBot*_`, m)
-if (/webp/g.test(mime)) out = await webp2png(img)
-else if (/image/g.test(mime)) out = await uploadImage(img)
-else if (/video/g.test(mime)) out = await uploadFile(img)
-if (typeof out !== 'string') out = await uploadImage(img)
-stiker = await sticker(false, out, global.packname, global.author)
-  
-}} else if (args[0]) {
-if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
-else return m.reply('*⚠️ EL ENLACE / URL / LINK NO ES VÁLIDO*')}
-
-if (stiker) {
-conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
-} else {
-console.log('*⚠️ RESPONDE A UNA IMAGEN O UN VIDEO PARA CONVERTIRLO EN STICKER*')
-console.log(stiker)
-}}
-
-handler.command = /^(s(tickers?)?(image|video|gif|img)?)$/i
-export default handler
-
-const isUrl = (text) => {
 return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))}
+

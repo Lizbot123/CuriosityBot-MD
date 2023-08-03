3819c25b69f9519f84ff3a26b10893efc1284c2c
Azami <137133333+Undefined17@users.noreply.github.com>
2023-08-03 10:57:55 -0600

    © CuriosityBot-MD

--- plugins/info-cuentas.js
+++ plugins/info-cuentas.js
@@ -1,16 +1,17 @@
 let media = [imagen5, imagen6, imagen7, imagen8, img1].getRandom()
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let str = `┏───◆ CUENTAS ◆───┓
-│ 🗃️ *GITHUB*
+let str = `
+┏───◆ CUENTAS ◆───┓
+│ *GITHUB*
 │ ${md}
-
-💌 *INSTAGRAM*
-*${ig}*
---------------------
-🚀 *YOUTUBE*
-*${yt}*
---------------------`
+┣────────────────
+│ *INSTAGRAM*
+│ ${ig}
+┣────────────────
+│ *YOUTUBE*
+│ ${yt}
+┗───────────────┛`
 
 await conn.sendFile(m.chat, media, 'gata.mp4', str, fkontak)}
 
